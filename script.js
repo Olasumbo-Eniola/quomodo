3c8893bb52113256d20bd4b9e96999e53d276ebb
Olasumbo-Eniola <olasumboeniola1@gmail.com>
2020-08-26 17:46:31 +0100

    quodomo

--- script.js
+++
@@ -1,9 +0,0 @@
-function Toggle() { 
-    var temp = document.getElementById("pswd"); 
-    if (temp.type === "password") { 
-        temp.type = "text"; 
-    } 
-    else { 
-        temp.type = "password"; 
-    } 
-} 
